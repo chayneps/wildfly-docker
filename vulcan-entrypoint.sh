5612b56884072439441af821c2167f0ecf36b0dd
Chayne P.S <csnamchaiskul@mobetize.com>
2019-04-19 07:15:33 -0700

    Fix bugs

--- vulcan-entrypoint.sh
+++ vulcan-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -d /mnt/logs/$MY_NODE_NAME ]; then
-  mkdir /mnt/logs/$MY_NODE_NAME
-  chmod 755 /mnt/logs/$MY_NODE_NAME
+if [ ! -d /mnt/logs/$MY_POD_NAME ]; then
+  mkdir /mnt/logs/$MY_POD_NAME
+  chmod 755 /mnt/logs/$MY_POD_NAME
 fi
-ln -s /mnt/logs/$MY_NODE_NAME /webapps/logs   
+ln -s /mnt/logs/$MY_POD_NAME /webapps/logs   
 /webapps/wildfly/bin/standalone.sh -b 0.0.0.0 > /webapps/logs/wildfly.log 2>&1
