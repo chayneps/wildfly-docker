1c6715ab928cde186276a2e18caea6da65729f1a
csnamchaiskul <49579049+csnamchaiskul@users.noreply.github.com>
2019-04-18 22:38:23 -0700

    Change log folder mode

--- vulcan-entrypoint.sh
+++ vulcan-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d /mnt/logs/$MY_NODE_NAME ]; then
   mkdir /mnt/logs/$MY_NODE_NAME
-  chmod 666 /mnt/logs/$MY_NODE_NAME
+  chmod 755 /mnt/logs/$MY_NODE_NAME
 fi
 ln -s /mnt/logs/$MY_NODE_NAME /webapps/logs   
 /webapps/wildfly/bin/standalone.sh -b 0.0.0.0 > /webapps/logs/wildfly.log 2>&1
